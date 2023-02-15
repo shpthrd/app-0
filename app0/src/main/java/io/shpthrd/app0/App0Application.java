147cac8918e30240ad87084e77bf50b1797d8c35
Shpthrd <shpthrd@protonmail.com>
2023-02-15 17:31:57 -0300

    api init

---
+++ app0/src/main/java/io/shpthrd/app0/App0Application.java
@@ -0,0 +1,13 @@
+package io.shpthrd.app0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App0Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(App0Application.class, args);
+	}
+
+}
---
+++ app0/src/test/java/io/shpthrd/app0/App0ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.shpthrd.app0;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class App0ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
